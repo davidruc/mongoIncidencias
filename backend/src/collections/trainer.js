7a6ec9d54885f253982cb425dd947208151992de
davidruc <davidarueda18@gmail.com>
2023-08-28 07:04:48 -0500

    feat: :sparkles: creation of the crud of trainer

---
+++ backend/src/collections/trainer.js
@@ -0,0 +1,67 @@
+import { collectionGen } from "../../db/connection.js";
+import siguienteId from "../helpers/Autoincrement.js";
+
+class Trainer{
+    _id;
+    id_trainer;
+    nombre;
+    rol;
+    permisos;
+    email_personal;
+    email_corporativo;
+    telefono_movil;
+    telefono_residencia;
+    telefono_empresa;
+    telefono_movil_empresarial;
+    cuenta_bancaria;
+    constructor(){}
+    async connection(){
+        try {
+            const result = await collectionGen("trainer");
+            return result;
+        } catch (error) {
+            throw error;
+        }
+    }
+    async getTrainers(id){
+        try {
+            const connection = await this.connection();
+            if(!id) return await connection.find({}).toArray();
+            return await connection.aggregate([{ $match:{ "id_trainer": parseInt(id)} }]).toArray();
+        } catch (error) {
+            throw error;
+        }
+    }
+    async postTrainer(data){
+        try {
+            const connection = await this.connection();
+            const newId = await siguienteId("trainer");
+            const result = await connection.insertOne({id_trainer: newId, ...data});
+            return result;
+        } catch (error) {
+            throw error;
+        }
+    };
+    async putTrainer(id, data){
+        try {
+            const connection = await this.connection();
+            const result = await connection.updateOne(
+                {"id_trainer": parseInt(id)}, 
+                {$set: data}
+            );
+            return result;
+        } catch (error) {
+            throw error;
+        }
+    }
+    async deleteTrainer(id){
+        try {
+            const connection = await this.connection();
+            const result = await connection.deleteOne({"id_trainer": parseInt(id)})
+            return result;
+        } catch (error) {
+            throw error; 
+        }
+    }
+}
+export { Trainer }
