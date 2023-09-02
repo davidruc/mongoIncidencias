a1bfd1cb17a8dd8b693e1a064c7654c2100fb26c
davidruc <davidarueda18@gmail.com>
2023-09-02 18:21:39 -0500

    feat: :sparkles: applie the dto to the put method

--- backend/src/routes/putData.js
+++ backend/src/routes/putData.js
@@ -2,15 +2,18 @@ import { Router } from "express";
 import * as controller from "../controllers/putDataController.js";
 import routesVersioning from "express-routes-versioning";
 import passportHelper from "../helpers/passportHelper.js";
+import { postValidateIncidencia, postValidateTrainer } from "../middleware/middlewareValidate.js";
+import { trainersPostDtoV1, incidenciasPostDtoV1 } from "../middleware/dataVerify.js";
+
 
 const putInitRoute = ()=>{
     const router = Router();
     const version = routesVersioning();
     router.use(passportHelper.authenticate("bearer", {session: false}));
-    router.put("/inicidencia", version({
+    router.put("/inicidencia", incidenciasPostDtoV1, postValidateIncidencia, version({
         "^1.0.0": controller.updateIncidenciaController
     }))
-    router.put("/trainer", version({
+    router.put("/trainer", trainersPostDtoV1, postValidateTrainer, version({
         "^1.0.0": controller.updateTrainerController
     }))
     return router;
